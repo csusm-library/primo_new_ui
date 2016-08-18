a4a546badb2f074f6d8f15f811c47affbc863280
Ian Chan <ichan@csusm.edu>
2016-08-18 16:04:23 -0700

    no message

--- CALS_USM/js/custom.js
+++ CALS_USM/js/custom.js
@@ -16,4 +16,41 @@
 
 })
 
+app.controller('prmLogoAfterController', [function () {
+
+  var vm = this;
+
+  vm.getIconLink = getIconLink;
+
+  function getIconLink() {
+
+  return vm.parentCtrl.iconLink;
+
+}
+
+}]);
+
+ 
+
+ 
+
+ 
+
+app.component('prmLogoAfter',{
+
+  bindings: {parentCtrl: '<'},
+
+  controller: 'prmLogoAfterController',
+
+  template: `<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner">
+
+  <a href="[your URL]">
+
+  <img class="logo-image" alt="{{::('nui.header.LogoAlt' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/>
+
+  </a>
+
+  </div>`
+
+});
    
