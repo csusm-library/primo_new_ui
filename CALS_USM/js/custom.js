5b143ad5d953deedadaeae8e1830b84b9869af78
Ian Chan <ichan@csusm.edu>
2016-08-22 08:26:18 -0700

    no message

--- CALS_USM/js/custom.js
+++ CALS_USM/js/custom.js
@@ -2,8 +2,35 @@
     "use strict";
     'use strict';
 
-
     var app = angular.module('viewCustom', ['angularLoad']);
+      app.controller('prmLogoAfterController', [function () {
+
+        var vm = this;
+
+        vm.getIconLink = getIconLink;
+
+        function getIconLink() {
+          return vm.parentCtrl.iconLink;
+        }
+      }]);
+
+      app.component('prmLogoAfter',{
+
+        bindings: {parentCtrl: '<'},
+
+        controller: 'prmLogoAfterController',
+
+        template: `<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner">
+
+        <a href="https://biblio.csusm.edu/">
+
+        <img class="logo-image" alt="{{::('nui.header.LogoAlt' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/>
+
+        </a>
+
+        </div>`
+
+      });
 
     /****************************************************************************************************/
 
@@ -13,44 +40,4 @@
 
     /****************************************************************************************************/
 
-
-})
-
-app.controller('prmLogoAfterController', [function () {
-
-  var vm = this;
-
-  vm.getIconLink = getIconLink;
-
-  function getIconLink() {
-
-  return vm.parentCtrl.iconLink;
-
-}
-
-}]);
-
- 
-
- 
-
- 
-
-app.component('prmLogoAfter',{
-
-  bindings: {parentCtrl: '<'},
-
-  controller: 'prmLogoAfterController',
-
-  template: `<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner">
-
-  <a href="[your URL]">
-
-  <img class="logo-image" alt="{{::('nui.header.LogoAlt' | translate)}}" ng-src="{{$ctrl.getIconLink()}}"/>
-
-  </a>
-
-  </div>`
-
-});
-   
+})();
